7b128c958463a90938aef622b3a0716e310b3824
OlexiiHubar <115034934+OlexiiHubar@users.noreply.github.com>
2023-04-21 17:05:17 +0300

    add Done 2 and 3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,26 @@
 import Player from '@vimeo/player';
+import _trottle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
 const onPlay = function (currentTime) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+  try {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(currentTime)
+    );
+  } catch (err) {
+    console.error(err.message);
+  }
 };
-
+player.on('timeupdate', _trottle(onPlay, 1000));
 function setCurrentTime() {
-  if (!'videoplayer-current-time') {
+  const data = localStorage.getItem('videoplayer-current-time');
+  if (!data) {
     return;
   }
+  const parseData = JSON.parse(data);
+  player.setCurrentTime(parseData.seconds);
 }
 setCurrentTime();
-player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import _trottle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+const formData = {};
+
+onPopulateFormData();
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', _trottle(onFormInput, 500));
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onPopulateFormData() {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedFormData) {
+    refs.email.value = savedFormData.email;
+    refs.textarea.value = savedFormData.message;
+  }
+}

39b4957c75fcaa9e105a78ccf0baa57719423a03
OlexiiHubar <115034934+OlexiiHubar@users.noreply.github.com>
2023-04-23 02:30:54 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,8 +4,8 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('input'),
-  textarea: document.querySelector('textarea'),
+  email: document.querySelector('input [name = "email"]'),
+  textarea: document.querySelector('textarea[name="message"]'),
 };
 
 const formData = {};
@@ -16,24 +16,31 @@ onPopulateFormData();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  if (formData.email === '' || formData.message === '') {
-    return alert('Поля мають бути заповнені');
+  const { email, message } = evt.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Поля мають бути заповнені!');
   }
+
+  const data = {
+    email: email.value,
+    message: message.value,
+  };
+
+  console.log(data);
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
-
 function onFormInput(e) {
   let formData = localStorage.getItem(STORAGE_KEY);
   if (formData) {
     formData = JSON.parse(formData);
   } else {
-    console.log(formData);
+    formData = {};
   }
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
 function onPopulateFormData() {
   const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
